5f34bc60c311e0a535d66e80b248da8393b83d57
David Scouten <39219635+zonemaster60@users.noreply.github.com>
2024-05-13 14:29:39 -0400

    updated to version 0.9.6.2
    updated on 05/13/2024

--- 2caf/Portable/2CAF_Portable.bat
+++ 2caf/Portable/2CAF_Portable.bat
@@ -9,7 +9,7 @@ REM BFCPEEMBEDDELETE=1
 REM BFCPEADMINEXE=1
 REM BFCPEINVISEXE=0
 REM BFCPEVERINCLUDE=1
-REM BFCPEVERVERSION=0.9.5.7
+REM BFCPEVERVERSION=0.9.6.2
 REM BFCPEVERPRODUCT=2Click Auto Fixer Portable
 REM BFCPEVERDESC=Uses built-in tools to fix file errors
 REM BFCPEVERCOMPANY=ZoneSoft
@@ -43,7 +43,7 @@ rem ********************
 Set chkhealth=False
 Set resetbase=False
 Set shutdown=False
-Set version=0.9.5.7
+Set version=0.9.6.2
 
 rem support files
 rem *************
@@ -51,6 +51,7 @@ Set logfile=Repairs.log
 Set inifile=Portable.ini
 Set exe1=%myfiles%\sfcfix.exe
 Set exe2=%myfiles%\w10mu.exe
+Set script1=scripts\appreinstall.bat
 
 rem set initial values
 rem ******************
@@ -511,30 +512,49 @@ Exit /B %ErrorLevel%
 
 :wTools
 Call :show_me %schcol1% 0
-rem PaintBoxAt 3 3 6 14 %schcol2%
+rem PaintBoxAt 3 3 7 14 %schcol2%
 rem PaintBoxAt 12 15 3 47 %schcol2%
 rem PrintColorAt "[  TOOLS ]" 4 5 7 %txtbg%
 rem PrintColorAt "[ SFCFIX ]" 5 5 %btnbg% %txtbg%
-rem PrintColorAt "[ UPDATE ]" 6 5 %btnbg% %txtbg%
-rem PrintColorAt "[ <<PREV ]" 7 5 %schcol4% %txtbg%
+rem PrintColorAt "[ UPDWIN ]" 6 5 %btnbg% %txtbg%
+rem PrintColorAt "[ POLICY ]" 7 5 %schcol4% %txtbg%
+rem PrintColorAt "[ <<PREV ]" 8 5 %schcol4% %txtbg%
 rem PrintColorAt "(WIP) Choose a TOOL, or <<PREV For MAINMENU" 13 17 %btnfg% %txtbg%
 
 rem button matrix
 rem *************
-rem MouseCmd 5,5,14,5 5,6,14,6 5,7,14,7
+rem MouseCmd 5,5,14,5 5,6,14,6 5,7,14,7 5,8,14,8
 
 If %result% EQU 1 (
 Call :make_button "[ SFCFIX ]" 5 5 1 10 %btnbg% %btntime% %txtbg%
 Cmd /c start %exe1%
+Call :click_next
 )
 
 If %result% EQU 2 (
-Call :make_button "[ UPDATE ]" 6 5 1 10 %btnbg% %btntime% %txtbg%
+Call :make_button "[ UPDWIN ]" 6 5 1 10 %btnbg% %btntime% %txtbg%
 Cmd /c start %exe2%
+Call :click_next
 )
 
 If %result% EQU 3 (
-Call :make_button "[ <<PREV ]" 7 5 1 10 %btnbg% %btntime% %txtbg%
+Call :make_button "[ -POLICY]" 7 5 1 10 %btnbg% %btntime% %txtbg%
+rem PrintColorAt "Resetting Policies to defaults..." 13 20 %btnfg% %txtbg%
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKLM\Software\Microsoft\WindowsSelfHost" /f
+reg delete "HKLM\Software\Policies" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Policies" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKCU\Software\Microsoft\WindowsSelfHost" /f
+reg delete "HKCU\Software\Policies" /f
+reg delete "HKLM\Software\Microsoft\Policies" /f
+Call :click_next
+GoTo wRestartNow
+)
+
+If %result% EQU 4 (
+Call :make_button "[ <<PREV ]" 8 5 1 10 %btnbg% %btntime% %txtbg%
 GoTo wMainMenu
 )
 GoTo wTools
